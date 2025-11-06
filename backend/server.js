629f94f9d84eaef571822830b0cd3ffa9eea61a8
Parimal Kashireddy <pkashi1@lsu.edu>
2025-11-06 01:51:55 -0600

    updated the server

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://salmon-meadow-0e430c50f.6.azurestaticapps.net/'],
+  origin: ['http://localhost:5173', 'https://salmon-meadow-0e430c50f.6.azurestaticapps.net'],
   credentials: false
 }));
 
